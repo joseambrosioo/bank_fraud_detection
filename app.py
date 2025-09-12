4003901354f9b4bf2323b4cbfdbc2015ee34dea3
jose-ambrosioo <josentambrosio@outlook.com>
2025-09-11 20:50:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -47,10 +47,10 @@ header = dbc.Navbar(
             html.Div(
                 [
                     html.Span("💰", className="me-2"),
-                    dbc.NavbarBrand("Fraud Detection on Bank Payments", class_name="fw-bold", style={"color": "black"}),
+                    dbc.NavbarBrand("Fraud Detection on Bank Payments", class_name="fw-bold text-wrap", style={"color": "black"}),
                 ], className="d-flex align-items-center"
             ),
-            dbc.Badge("Interactive Dashboard", color="primary", className="ms-auto")
+            dbc.Badge("Dashboard", color="primary", className="ms-auto")
         ]
     ),
     color="light",
